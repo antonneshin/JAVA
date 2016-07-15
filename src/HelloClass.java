8e0fa858f63c7b1d5e88005cc461c1e133f5d1fc
antonneshin <antokha333@yandex.ru>
2016-07-15 18:56:34 +0300

    first class in java

---
+++ src/HelloClass.java
@@ -0,0 +1,10 @@
+package src;
+
+/**
+ * Created by Student on 15.07.2016.
+ */
+public class HelloClass {
+    public static void main( String[] args) {
+        System.out.println("Hello,world!!!!!");
+    }
+}
