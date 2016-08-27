e8ab3df884e2d2b874077b58488152f0c33175c6
antonneshin <antokha333@yandex.ru>
2016-08-28 01:00:15 +0400

    homeWork 25/08/2016 added second task

--- src/home/sbt/home2508/ExecutionManager.java
+++ src/home/sbt/home2508/ExecutionManager.java
@@ -1,9 +1,11 @@
 package src.home.sbt.home2508;
 
+import java.util.Vector;
+
 /**
  * Created by Пентагон on 25.08.2016.
  */
 public interface ExecutionManager {
-    Context execute(Runnable callback, Runnable... tasks);
+    Context execute(Runnable callback, Vector tasks);
 }
 
--- src/home/sbt/home2508/Main.java
+++ src/home/sbt/home2508/Main.java
@@ -1,5 +1,7 @@
 package src.home.sbt.home2508;
 
+import java.util.Vector;
+
 /**
  * Created by Пентагон on 25.08.2016.
  */
@@ -8,6 +10,34 @@ package src.home.sbt.home2508;
 
 public class Main {
     public static void main(String[] args) {
-
+        ExecutionManager executionManager=null;
+        try {
+            executionManager = new ExecutionManagerImpl(3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Vector<Runnable> runs=new Vector<>();
+        for (int i=0;i<10;i++){
+            Runnable r =new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("---some work");
+                }
+            };
+            runs.add(r);
+        }
+        Context context = executionManager.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(888);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("callback is in process...");
+            }
+        },runs);
+        while(!context.isFinished()){}
+        context.interrupt();
     }
 }
