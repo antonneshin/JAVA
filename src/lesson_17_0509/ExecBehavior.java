c50b75d542bd6f4d9a74a31745d92c3d19d13fd6
antonneshin <antokha333@yandex.ru>
2016-09-05 21:00:30 +0300

    05-09-2016 lesson

---
+++ src/lesson_17_0509/ExecBehavior.java
@@ -0,0 +1,8 @@
+package src.lesson_17_0509;
+
+/**
+ * Created by Student on 05.09.2016.
+ */
+public interface ExecBehavior {
+    void exec(Document doc);
+}
---
+++ src/lesson_17_0509/IDocument.java
@@ -0,0 +1,13 @@
+package src.lesson_17_0509;
+
+/**
+ * Created by Student on 05.09.2016.
+ */
+public interface IDocument {
+
+    public void print();
+
+    public void printUserInfo();
+
+    public void saveToDB();
+}
