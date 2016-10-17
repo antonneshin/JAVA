f2ffa681d6dbe7168318e2b1516fb974a408fd7e
antonneshin <antokha333@yandex.ru>
2016-10-17 20:35:42 +0300

    reactJS - 17/10/2016

---
+++ src/reactjs/main.js
@@ -0,0 +1,7 @@
+/** Created by batmah on 16.10.16. */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+ReactDOM.render(<App target="JAVA"> hello </App>, document.getElementById('app'));
